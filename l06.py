03329ab635d4ba01c2fb0b16d771fa6d805c8450
lppduy <lephamphuongduy@gmail.com>
2023-08-26 16:11:25 +0700

    l06

---
+++ l06.py
@@ -0,0 +1,17 @@
+# Phép toán logic: and or not 
+
+
+a = 19
+b = 5
+
+# and: trả về True nếu cả 2 điều kiện bên trái và bên phải nó đúng, ngược lại trả về False
+
+print(a>=0 and a<=10)
+
+# or: trả về True nếu chỉ cần 1 trong 2 điều kiện bên trái hoặc bên phải của nó đúng, ngược lại trả vè False
+print(a>=0 or a<=10)
+
+# not: biến True thành False, biến False thành True
+
+print(not(a>=0 or a<=10))
+
