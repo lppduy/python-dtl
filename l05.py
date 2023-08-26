205ab350d9bc195016e5c7e20207cabd80cf1ad2
lppduy <lephamphuongduy@gmail.com>
2023-08-26 16:05:50 +0700

    l05

---
+++ l05.py
@@ -0,0 +1,13 @@
+# Phép toán so sánh:
+# > < >= <= == !=
+# Kết quả của phép toán so sánh là True hoặc False tuỳ thuộc vào việc so sánh là đúng hay sai
+
+a = 9
+b = 5
+
+print(a > b)
+print(a > b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
