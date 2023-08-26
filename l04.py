c8e2a0ad6306e35ed0dc22a309794227b187eba6
lppduy <lephamphuongduy@gmail.com>
2023-08-26 16:03:16 +0700

    l04

---
+++ l04.py
@@ -0,0 +1,24 @@
+# Phép toán số học:
+a = 9
+b = 5
+# + - * /
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+# (//) chia nguyên
+print(a // b)
+
+# % chia dư
+print(a % b)
+
+# ** luỹ thừa
+print(a ** 2)
+print(a ** 0.5) 
+
+# Thay đổi giá trị của biến
+a += 3
+print(a)
+# -=, *=, /=, //=, **=
+a**=2
+print(a)
